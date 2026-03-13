d650bd69b94037e66cd0b7f5494ecd4e07b5cad0
Rhonn Walter <rhonnwalter06@gmail.com>
2026-03-13 23:30:28 +0800

    add GeneratePayrollForm

--- tasks/forms.py
+++ tasks/forms.py
@@ -10,7 +10,25 @@ class PayrollForm(forms.ModelForm):
             'hours_worked',
             'overtime_hours',
         ]
+
+class GeneratePayrollForm(forms.Modelform):
+    start_date = forms.DateField(
+        widget=forms.DateInput(attrs={'type':'date'}),
+        label="Payroll_Start_Date"
+    )
+    end_date = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        label="Payroll_End_date"
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start = cleaned_data.get("start_date")
+        end = cleaned_data.get("end_date")
             
+        if start and end and start > end:
+            raise forms.ValidationError("Start date cannot be after the end date")
+        
 class EmployeeForm(forms.ModelForm):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
