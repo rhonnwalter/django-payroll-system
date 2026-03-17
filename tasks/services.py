881be9b8266a723ad7bd2b04cfd4f5b22277501f
Rhonn Walter <rhonnwalter06@gmail.com>
2026-03-18 00:14:36 +0800

    feat: income tax computation

---
+++ tasks/services.py
@@ -0,0 +1,51 @@
+from decimal import Decimal, ROUND_HALF_UP
+from django.db.models import F,Case,When,ExpressionWrapper, DecimalField, Value
+
+def compute_total_pay(employee, total_regular_hours, total_overtime_hours):
+        
+        if employee.pay_type == "hourly":
+            total = (
+                total_regular_hours * employee.hourly_rate +
+                total_overtime_hours * employee.overtime_multiplier
+            )
+        elif employee.pay_type == "salary":
+            total = employee.salary_per_period
+        else:
+            total = Decimal('0.00')
+
+        return total.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP) #sets the precision as 2. decimal places.
+
+def total_pay_expression():
+        return ExpressionWrapper(
+            Case(
+                When(employee__pay_type="hourly",
+                     then=(F('total_regular_hours')  * F('employee__hourly_rate') +
+                     F('total_overtime_hours') * F('employee__overtime_multiplier')
+                    )
+                ),
+                When(employee__pay_type="salary",
+                    then=(F('employee__salary_per_period') 
+                    ) 
+                ),
+                default=Value(Decimal('0.00'))
+            
+            ), 
+            output_field=DecimalField(max_digits=10, decimal_places=2)  
+         
+         )
+
+
+def compute_income_tax(gross_pay):
+      
+      if gross_pay <= 20833:
+            return Decimal("0.00")
+      elif gross_pay <= 33332:
+            return (gross_pay - Decimal("20833")) * Decimal("0.15")
+      elif gross_pay <= 66666:
+            return (gross_pay - Decimal("33332")) * Decimal("0.20")
+      elif gross_pay <= 166666:
+            return (gross_pay - Decimal("66666")) * Decimal("0.25")
+      elif gross_pay > 666667:
+            return (gross_pay - Decimal("166666")) * Decimal("0.30")
+      else:
+        return Decimal("183541.67") + (gross_pay - Decimal("666667")) * Decimal("0.35")
