bdee34ce00c15aaf2a8844545ad238099fc52b32
Rhonn Walter <rhonnwalter06@gmail.com>
2026-03-26 22:48:41 +0800

    refactor: urls.py arrangement and names

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,15 +7,19 @@ urlpatterns = [
     path('dashboard/employee', views.employee_dashboard, name='employee_dashboard'),
     path('dashboard/hr', views.hr_dashboard, name='hr_dashboard'),
 
+    path('my-attendace/', views.my_attendance, name='my_attendace'),
     path('attendace-detail/<int:pk', views.attendace_detail, name='attendace_detail'),
+   
+
     path('my-payroll/', views.my_payroll, name='my_payroll'),
-    path('payrolls/<int:pk>/', views.payroll_detail, name='payroll_detail'),
+    path('payroll-detail/<int:pk>/', views.payroll_detail, name='payroll_detail'),
     path('payroll-history/<int:employee_id>/', views.payroll_history, name='payroll_history'),
+
     path('hr/attendace-list/', views.attendance_list, name='attendance_list'),
-    path('hr/employees/', views.employee_list, name='employee_list'),
-    path('hr/employee/<int:employee_id>/payrolls/',views.employee_payrolls, name='employee_payrolls'),
-    
-    path('hr/payrolls/', views.hr_payroll_list, name='hr_payroll_list'),
+    path('hr/employees-list/', views.employee_list, name='employee_list'),
+    path('hr/employees/<int:employee_id>/payrolls/',views.employee_payrolls, name='employee_payrolls'),
+    path('hr/payrolls-list/', views.hr_payroll_list, name='hr_payroll_list'),
+
     path('hr/create-employee/', views.create_employee, name='create_employee'),
     path('hr/mark-paid/<int:pk>', views.mark_paid, name='mark_paid'),
     
