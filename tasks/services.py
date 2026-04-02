059620f10658d786c21ec2bbb4cd3f891c6c41f7
Rhonn Walter <rhonnwalter06@gmail.com>
2026-04-02 15:51:04 +0800

    refactor: moved filtering logic of attendace list to services.py

--- tasks/services.py
+++ tasks/services.py
@@ -15,3 +15,31 @@ def filter_employees(queryset, search=None):
         )
         queryset = queryset.filter(search_condition)
     return queryset
+
+def filter_attendances(queryset, search=None, date_from=None, date_to=None):
+
+    if date_from:
+        try: 
+            date_from = datetime.strptime(date_from, "%Y-%m-%d").date()
+            queryset = queryset.filter(date__gte=date_from)
+        except ValueError: 
+            date_from = None
+            
+    if date_to:
+        try: 
+            date_to = datetime.strptime(date_to, "%Y-%m-%d").date()
+            queryset = queryset.filter(date__lte=date_to)
+        except ValueError:
+            date_to = None
+
+    if search:
+        search_condition = (
+            Q(employee__user__username__icontains=search) |
+            Q(employee__user__first_name__icontains=search) |
+            Q(employee__user__last_name__icontains=search) |
+            Q(employee__department__icontains=search) 
+        )
+        queryset = queryset.filter(search_condition)
+        
+    return queryset
+        
