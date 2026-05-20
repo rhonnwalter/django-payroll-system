6cf35653ab862d63ec80a3a795adfa4d025f0406
Rhonn Walter <rhonnwalter06@gmail.com>
2026-05-20 18:04:03 +0800

    removed redundant annotate

--- tasks/services/hr_dashboard.py
+++ tasks/services/hr_dashboard.py
@@ -1,13 +1,13 @@
 from tasks.models import Payroll, Employee
 from django.db.models import Sum
 
-def hr_dashboard_stats():
-    payrolls = Payroll.objects.annotate(total_pay_expr=Payroll.total_pay_expression())
+def hr_dashboard_stats(request):
+    payrolls = Payroll.objects.all()
     return {
         'payrolls' : payrolls,
         'total_employees' : Employee.objects.count(),
         'total_payrolls' : payrolls.count(),
-        'total_salary' : payrolls.aggregate(Sum('total_pay_expr'))['total_pay_expr__sum'] or 0 
+        'total_salary' : payrolls.aggregate(Sum('net_pay'))
         #['total_pay_expr__sum' grab the actual number from the dictionary
     }
   
