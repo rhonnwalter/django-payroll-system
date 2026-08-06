df440f84ed052cc2b85116937877f1894625f90a
Rhonn Walter <rhonnwalter06@gmail.com>
2026-08-06 15:44:49 +0800

    add filter_positions_by_department function

--- tasks/services/employee_services.py
+++ tasks/services/employee_services.py
@@ -1,13 +1,18 @@
 from django.db.models import Q
 from django.db import transaction
 from django.contrib.auth.models import User
-from tasks.models import Employee, Department
+from tasks.models import Employee, Department, Position
 
 def get_base_employee():
     return Employee.objects.select_related('user').filter(is_active=True)
 
 def get_department(dept_id=None):
     return Department.objects.get(id=dept_id)
+
+def filter_positions_by_department(dept_id=None):
+    return Position.objects.filter(department_id=dept_id).values('id', 'title')
+
+
 
 def filter_employees(queryset, search=None):
     if search:
@@ -44,5 +49,6 @@ def create_employee_service(form):
     return employee
 
     
-    
+def department_list():
+    return Department.objects.all()
    
