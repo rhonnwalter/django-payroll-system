6d01832873c84f4a039bd02f3a24f73529e1271b
Rhonn Walter <rhonnwalter06@gmail.com>
2026-08-10 16:33:29 +0800

    add media_url for uploading profile_picture

--- config/settings.py
+++ config/settings.py
@@ -118,5 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 LOGIN_REDIRECT = '/dashboard/'
 LOGIN_URL = '/admin/login/'
